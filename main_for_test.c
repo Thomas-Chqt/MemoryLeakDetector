1a59f81e9d4e7c459d89b7ae8d1e312792b443e8
Thomas <tchoquet@student.42tokyo.jp>
2023-07-03 15:32:02 +0900

    update main_for_test.c

--- main_for_test.c
+++ main_for_test.c
@@ -6,7 +6,7 @@
 /*   By: tchoquet <tchoquet@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/02 20:15:11 by tchoquet          #+#    #+#             */
-/*   Updated: 2023/07/03 14:04:53 by tchoquet         ###   ########.fr       */
+/*   Updated: 2023/07/03 15:31:40 by tchoquet         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,5 @@ int main()
 {
 	void *a = malloc(100);
 	void *b = malloc(100);
-	void *c = ft_calloc(1, 400);
-	void *d = ft_calloc(5, 400);
-
-	free(b);
-	free(a);
 	return 0;
 }
