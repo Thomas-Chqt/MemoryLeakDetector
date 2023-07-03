ddeb02adabbbded01dd99db494b3adb7282f190d
Thomas <tchoquet@student.42tokyo.jp>
2023-07-03 15:26:22 +0900

    remove libft

--- includes/memory_leak_detector.h
+++ includes/memory_leak_detector.h
@@ -15,27 +15,35 @@
 
 # include <libc.h>
 
-# include <libft.h>
+typedef enum e_bool			t_bool;
+typedef struct s_list		t_list;
+
+enum e_bool { false = 0, true = 1 };
+
+struct s_list
+{
+	void	*data;
+	t_list	*next;
+};
 
 typedef struct s_mem
 {
     size_t		address;
     size_t		size;
-    t_uint32	line;
+    uint32_t	line;
 	char		*file;
 
 } t_mem;
 
 
-void *_malloc(size_t size, t_uint32 line, char *file);
-void *_ft_calloc(size_t count, size_t size, t_uint32 line, char *file);
+void *_malloc(size_t size, uint32_t line, char *file);
+void *_ft_calloc(size_t count, size_t size, uint32_t line, char *file);
 void _free(void *ptr);
 void print_report();
 
 # ifndef NO_REDEFINE
 
 #  define malloc(size) _malloc(size, __LINE__, __FILE__)
-// #  define ft_calloc(count, size) _ft_calloc(count, size, __LINE__, __FILE__)
 #  define free(ptr) _free(ptr)
 
 # endif // NO_REDEFINE
--- sources/memory_leak_detector.c
+++ sources/memory_leak_detector.c
@@ -7,13 +7,79 @@ t_list	*allocated_mem = NULL;
 size_t	g_total_alloc = 0;
 size_t	g_total_free = 0;
 
+void	ft_lstadd_front(t_list **lst, t_list *new)
+{
+	if (lst == NULL)
+		return ;
+	new->next = (*lst);
+	(*lst) = new;
+}
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*new_lst;
+
+	new_lst = malloc(sizeof(t_list));
+	if (new_lst == NULL)
+		return (NULL);
+	new_lst->data = content;
+	new_lst->next = NULL;
+	return (new_lst);
+}
+
+void	ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	t_list	*watched;
+	t_list	*temp;
+
+	watched = *lst;
+	while (watched != NULL)
+	{
+		del(watched->data);
+		temp = watched->next;
+		free(watched);
+		watched = temp;
+	}
+	(*lst) = NULL;
+}
+
+void	lst_delete_if(t_list **head, void (*del)(void *),
+			t_bool (*condition)(void *, void *), void *data)
+{
+	t_list	*current;
+	t_list	*temp;
+
+	if (head == NULL || (*head) == NULL || condition == NULL || del == NULL)
+		return ;
+	if (condition((*head)->data, data) == true)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		del(temp->data);
+		free(temp);
+	}
+	current = *head;
+	while (current->next != NULL)
+	{
+		if (condition(current->next->data, data) == true)
+		{
+			temp = current->next;
+			current->next = current->next->next;
+			del(temp->data);
+			free(temp);
+		}
+		else
+			current = current->next;
+	}
+}
+
 void print_free(void *mem)
 {
     printf("Memory leak at line %d in file %s: (%lu bytes)\n", ((t_mem *)mem)->line, ((t_mem *)mem)->file, ((t_mem *)mem)->size);
 	free(mem);
 }
 
-void insert(size_t address, size_t size, t_uint32 line, char *file)
+void insert(size_t address, size_t size, uint32_t line, char *file)
 {
 	t_mem	*new_mem = malloc(sizeof(t_mem));
 	if (new_mem == NULL)
@@ -50,23 +116,13 @@ void print_report() {
 	ft_lstclear(&allocated_mem, &print_free);
 }
 
-void *_malloc(size_t size, t_uint32 line, char *file)
+void *_malloc(size_t size, uint32_t line, char *file)
 {
     void *ptr = malloc(size);
 	if (ptr == NULL)
 		printf("Allocation error line %d int file %s", line, file);
 	else
     	insert((size_t)ptr, size, line, file);
-    return ptr;
-}
-
-void *_ft_calloc(size_t count, size_t size, t_uint32 line, char *file)
-{
-	void *ptr = ft_calloc(count, size);
-	if (ptr == NULL)
-		printf("Allocation error line %d int file %s", line, file);
-	else
-    	insert((size_t)ptr, count * size, line, file);
     return ptr;
 }
 
